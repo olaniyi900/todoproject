c93bbc2ebaf44bf7e115056cff994aaf2f5a78ce
olaniyi <ajayiolaniyi8@gmail.com>
2020-05-18 18:01:19 +0100

    create list of todo

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Todo
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'base.html')
+
+
+def todoList(request):
+    qs = Todo.objects.all()
+    template_name = 'todoapp/todolist.html'
+    context = {'todos':qs }
+    return render(request, template_name, context)
